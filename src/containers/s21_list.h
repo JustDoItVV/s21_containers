f39327ba1924ccba4a77fab2d9590fbea87e3e15
Kirill <ogdengum@student.21-school.ru>
2024-05-31 16:50:54 +0300

    feat: all from specifications added + mocks

--- src/containers/s21_list.h
+++ src/containers/s21_list.h
@@ -7,27 +7,50 @@
 #include <utility>
 
 namespace s21 {
-template <class T>
-class list {
- public:
-  // Queue Member type
-  using value_type = T;
-  using reference = T &;
-  using const_reference = const T &;
-  using size_type = size_t;
+  template <class T>
+  class list {
+  public:
+    // list member type
+    using value_type = T;
+    using reference = T &;
+    using const_reference = const T &;
+    using size_type = size_t;
+    using iterator = ListIterator<T>;
+    using const_iterator = ListConstIterator<T>;
 
-  // using iterator = ListIterator<T>;
-  // using const_iterator = ListConstIterator<T>;
+    // list functions
+    list() {}
+    list(size_type n) {}
+    list(std::initializer_list<value_type> const &items) {}
+    list(const list &l) {}
+    list(list &&l) {}
+    ~list() {}
+    list operator=(list &&l) {}
 
- public:
-  list() {}
-  list(size_type n) {}
-  list(std::initializer_list<value_type> const &items) {}
-  list(const list &l) {}
-  list(list &&l) {}
-  ~list() {}
-  list operator=(list &&l) {}
-};
+    // list iterators
+    iterator begin() {return NULL;}
+    iterator end() {return NULL;}
+
+    // list capacity
+    bool empty() {return true;}
+    size_type size() {return 0;}
+    size_type maxsize() {return 0;}
+
+    // list modifiers
+    void clear() {}
+    iterator insert(iterator pos, const_reference value) {return NULL;}
+    void erase (iterator pos) {}
+    void push_back (const_reference value) {}
+    void pop_back() {}
+    void push_front(const_reference value) {}
+    void pop_front() {}
+    void swap (list& other) {}
+    void merge (list& other) {}
+    void splice(const_iterator pos, list& other) {}
+    void reverse() {}
+    void unique() {}
+    void sort() {}
+  };
 
 };  // namespace s21
 
