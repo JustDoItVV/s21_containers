b30d727deb6304bbb4aba69d8402e7b4e32b2cfa
Magic <dn_den@mail.ru>
2024-06-06 07:53:33 +0300

    feat: multiset tests and part of implimentation

--- src/containers/s21_multiset.h
+++ src/containers/s21_multiset.h
@@ -15,42 +15,86 @@ class multiset : public RBTree<Key, Key> {
   using iterator = typename RBTree<Key, Key>::Iterator;
   using const_iterator = const iterator;
   using size_type = size_t;
+  using Node = typename RBTree<Key, Key>::Node;
 
  public:
   // Member functions
   multiset() : RBTree<Key, Key>(){};
-  multiset(std::initializer_list<value_type> const& items) {}
+  multiset(std::initializer_list<value_type> const& items) {
+    for (value_type value : items) insert(value);
+  }
   multiset(const multiset& ms) {}
   multiset(multiset&& ms) {}
   ~multiset() {}
   multiset operator=(multiset&& ms) {}
 
   // Iterators
-  iterator begin() { return NULL; }
-  iterator end() { return NULL; }
+  // iterator begin() { return NULL; }
+  // iterator end() { return NULL; }
 
   // Capacity
-  bool empty() { return false; }
-  size_type size() { return 0; }
-  size_type max_size() { return 0; }
+  // bool empty() { return false; }
+  // size_type size() { return 0; }
+  // size_type max_size() { return 0; }
 
   // Modifiers
-  void clear() {}
-  iterator insert(const value_type& value) { return NULL; }
-  void erase(iterator pos) {}
-  void swap(multiset& other) {}
-  void merge(multiset& other) {}
+  // void clear() {}
+  iterator insert(const value_type& value) {
+    Node* node = new Node();
+    node->key = value;
+    node->value = value;
+
+    return insertNode(node);
+  }
+  void merge(multiset& other) {
+    RBTree constTree(other);
+    iterator it = constTree.begin();
+    iterator itEnd = other.end();
+
+    for (; it != itEnd; ++it) iterator insertResult = insert(*it);
+    other.clear();
+  }
+  // void erase(iterator pos) {}
+  // void swap(multiset& other) {}
+  // void merge(multiset& other) {}
 
   // Lookup - просмотр мультисета
   size_type count(const Key& key) { return 0; }
   iterator find(const Key& key) { return NULL; }
-  bool contains(const Key& key) { return false; }
+  // bool contains(const Key& key) { return false; }
   std::pair<iterator, iterator> equal_range(const Key& key) {
     return std::pair<iterator, iterator>(nullptr, nullptr);
   }
   iterator lower_bound(const Key& key) { return NULL; }
   iterator upper_bound(const Key& key) { return NULL; }
+
+  iterator insertNode(Node* node) {
+    Node *parent = nullptr, *root = this->root;
+    node->color = RBTree<Key, Key>::RED;
+
+    while (root != nullptr) {
+      parent = root;
+      if (node->key < root->key)
+        root = root->left;
+      else
+        root = root->right;
+    };
+
+    if (parent == nullptr)
+      this->root = node;
+    else if (node->key < parent->key)
+      parent->left = node;
+    else
+      parent->right = node;
+
+    node->parent = parent;
+
+    RBTree<Key, Key>::fixInsertion(node);
+
+    return node;
+  };
 };
+
 };  // namespace s21
 
 #endif
--- src/containers_tests/s21_multiset_test.cpp
+++ src/containers_tests/s21_multiset_test.cpp
@@ -70,3 +70,157 @@ TEST(multiset, moveConstructor) {
   for (; s21Iter != s21IterEnd; ++s21Iter, ++stlIter)
     EXPECT_EQ(*s21Iter, *stlIter);
 }
+
+// TEST(multiset, begin) {
+//   s21::multiset<int> s21Multiset{0, 1, 2, 3, 3, 1, 6, 7, 6};
+//   std::multiset<int> stlMultiset{0, 1, 2, 3, 3, 1, 6, 7, 6};
+//   auto s21Iter = s21Multiset.begin();
+//   auto stlIter = stlMultiset.begin();
+
+//   EXPECT_EQ(*s21Iter, *stlIter);
+//   s21Iter++;
+//   stlIter++;
+//   EXPECT_EQ(*s21Iter, *stlIter);
+// }
+
+// TEST(multiset, end) {
+//   s21::multiset<int> s21Multiset{0, 1, 2, 3, 3, 1, 6, 7, 6};
+//   std::multiset<int> stlMultiset{0, 1, 2, 3, 3, 1, 6, 7, 6};
+//   auto s21Iter = s21Multiset.end();
+//   auto stlIter = stlMultiset.end();
+
+//   s21Iter--;
+//   stlIter--;
+//   EXPECT_EQ(*s21Iter, *stlIter);
+// }
+
+TEST(multiset, empty) {
+  s21::multiset<int> s21EmptyMultiset{};
+  std::multiset<int> stlEmptyMultiset{};
+  EXPECT_EQ(s21EmptyMultiset.empty(), stlEmptyMultiset.empty());
+}
+
+TEST(multiset, size) {
+  s21::multiset<int> s21Multiset{};
+  std::multiset<int> stlMultiset{};
+
+  EXPECT_EQ(s21Multiset.size(), stlMultiset.size());
+  s21Multiset.insert(1);
+  stlMultiset.insert(1);
+  EXPECT_EQ(s21Multiset.empty(), stlMultiset.empty());
+  EXPECT_EQ(s21Multiset.size(), stlMultiset.size());
+}
+
+TEST(multiset, max_size) {
+  s21::multiset<int> s21Multiset{};
+  auto maxSize = s21Multiset.max_size();
+  EXPECT_GT(maxSize, 0);
+}
+
+TEST(multiset, clear) {
+  s21::multiset<int> s21Multiset{};
+  std::multiset<int> stlMultiset{};
+
+  EXPECT_EQ(s21Multiset.empty(), stlMultiset.empty());
+  EXPECT_EQ(s21Multiset.size(), stlMultiset.size());
+
+  s21Multiset.insert(1);
+  stlMultiset.insert(1);
+  EXPECT_EQ(s21Multiset.empty(), stlMultiset.empty());
+  EXPECT_EQ(s21Multiset.size(), stlMultiset.size());
+
+  s21Multiset.clear();
+  stlMultiset.clear();
+  EXPECT_EQ(s21Multiset.empty(), stlMultiset.empty());
+  EXPECT_EQ(s21Multiset.size(), stlMultiset.size());
+}
+
+TEST(multiset, insert) {
+  s21::multiset<int> s21Multiset{0, 1, 2, 3, 3, 1, 6, 7, 6};
+  std::multiset<int> stlMultiset{0, 1, 2, 3, 3, 1, 6, 7, 6};
+
+  auto s21InsertResult = s21Multiset.insert(4);
+  auto stlInsertResult = stlMultiset.insert(4);
+  EXPECT_EQ(s21Multiset.count(4), stlMultiset.count(4));
+  EXPECT_TRUE(*s21InsertResult == *stlInsertResult);
+
+  s21InsertResult = s21Multiset.insert(1);
+  stlInsertResult = stlMultiset.insert(1);
+  EXPECT_EQ(s21Multiset.count(1), stlMultiset.count(1));
+  EXPECT_TRUE(*s21InsertResult == *stlInsertResult);
+}
+
+// TEST(multiset, erase) {
+//   s21::multiset<int> s21Multiset{0, 1, 2, 3, 3, 1, 6, 7, 6};
+//   std::multiset<int> stlMultiset{0, 1, 2, 3, 3, 1, 6, 7, 6};
+//   auto size = s21Multiset.size();
+//   auto itEnd = s21Multiset.end();
+//   itEnd--;
+//   s21Multiset.erase(itEnd);
+//   auto new_size = s21Multiset.size();
+
+//   EXPECT_NE(size, new_size);
+//   s21Multiset.erase(s21Multiset.begin());
+//   stlMultiset.erase(stlMultiset.begin());
+//   auto s21Iter = s21Multiset.begin();
+//   auto stlIter = stlMultiset.begin();
+//   auto s21IterEnd = s21Multiset.end();
+
+//   for (; s21Iter != s21IterEnd; ++s21Iter, ++stlIter)
+//     EXPECT_EQ(*s21Iter, *stlIter);
+// }
+
+TEST(multiset, swap) {
+  s21::multiset<int> s21Multiset{0, 1, 2};
+  s21::multiset<int> s21SwapMultiset{3, 3, 4, 5, 7, 6, 6};
+
+  s21Multiset.swap(s21SwapMultiset);
+  EXPECT_EQ(s21Multiset.size(), 7);
+  EXPECT_EQ(s21SwapMultiset.size(), 3);
+  EXPECT_EQ(*s21Multiset.begin(), 3);
+  EXPECT_EQ(*s21SwapMultiset.begin(), 0);
+}
+
+TEST(multiset, merge) {
+  s21::multiset<int> s21Multiset{0, 1, 2};
+  s21::multiset<int> s21MergeMultiset{3, 3, 4, 5, 7, 6, 6};
+  s21Multiset.merge(s21MergeMultiset);
+  std::multiset<int> stlMultiset{0, 1, 2};
+  std::multiset<int> stlMergeMultiset{3, 3, 4, 5, 7, 6, 6};
+  stlMultiset.merge(stlMergeMultiset);
+
+  auto s21Iter = s21Multiset.begin();
+  auto stlIter = stlMultiset.begin();
+  auto s21IterEnd = s21Multiset.end();
+  for (; s21Iter != s21IterEnd; ++s21Iter, ++stlIter)
+    EXPECT_EQ(*s21Iter, *stlIter);
+  EXPECT_EQ(stlMultiset.size(), s21Multiset.size());
+  EXPECT_EQ(s21MergeMultiset.size(), stlMergeMultiset.size());
+}
+
+TEST(multiset, count) {
+  // TODO: do it
+}
+
+TEST(multiset, find) {
+  s21::multiset<int> s21Multiset{0, 1, 2, 3, 3, 1, 6, 7, 6};
+  std::multiset<int> stlMultiset{0, 1, 2, 3, 3, 1, 6, 7, 6};
+
+  auto s21Iter = s21Multiset.find(3);
+  auto stlIter = stlMultiset.find(3);
+  auto s21Iter2 = s21Multiset.find(4);
+  auto stlIter2 = stlMultiset.find(4);
+
+  EXPECT_EQ(*s21Iter, *stlIter);
+  EXPECT_EQ(*s21Iter2, *stlIter2);
+}
+
+TEST(multiset, contains) {
+  s21::multiset<int> s21Multiset{0, 1, 2, 3, 3, 1, 6, 7, 6};
+
+  auto isContains1 = s21Multiset.contains(4);
+  auto isContains2 = s21Multiset.contains(1);
+
+  EXPECT_EQ(isContains1, false);
+  EXPECT_EQ(isContains2, true);
+}
