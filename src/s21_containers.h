50ee78f51411404f57399c481cbb7619d925d545
Lilian <winterchatten@gmail.com>
2024-06-07 20:12:58 +0300

    feat: comment deleted

--- src/s21_containers.h
+++ src/s21_containers.h
@@ -5,7 +5,7 @@
 #include "containers/s21_map.h"
 #include "containers/s21_queue.h"
 #include "containers/s21_set.h"
-// #include "containers/s21_stack.h"
+#include "containers/s21_stack.h"
 #include "containers/s21_vector.h"
 
 #endif
